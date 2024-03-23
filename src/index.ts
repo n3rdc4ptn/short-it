ee94f9f1a7ee7146649f58d237cd40d72b062c95
Moritz Reich <moritz.reich@minest.de>
2024-03-23 11:48:51 +0100

    created url shortener

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,91 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { UnkeyContext, unkey } from '@unkey/hono';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+export type Env = {
+	UNKEY_API_ID: string;
+	DEFAULT_EXPIRATION: number;
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+	SHORT_IT: KVNamespace;
 };
+
+const app = new Hono<{ Bindings: Env; Variables: { unkey: UnkeyContext } }>();
+
+app.get('/:slug', async (c) => {
+	const { slug } = c.req.param();
+	const url = await c.env.SHORT_IT.get(slug);
+
+	if (url === null) {
+		return new Response('Not Found', { status: 404 });
+	}
+
+	return Response.redirect(url, 307);
+});
+
+// Secured post endpoint
+app.use(
+	'/*',
+	async (c, next) => {
+		// if (c.req.method === 'GET') {
+		// 	return next();
+		// }
+
+		const unkeyMiddleware = unkey({
+			apiId: c.env.UNKEY_API_ID,
+		});
+
+		return unkeyMiddleware(c, next);
+	},
+	async (c, next) => {
+		if (c.var.unkey.valid === false) {
+			return new Response('Unauthorized', { status: 401 });
+		}
+
+		await next();
+	}
+);
+
+app.get('/', async (c) => {
+	const all = await c.env.SHORT_IT.list();
+
+	const keys = all.keys.map((key) => key.name);
+
+	return c.json(keys);
+});
+
+app.post('/', async (c) => {
+	const url = await c.req.text();
+	const slug = Math.random().toString(36).slice(2);
+
+	await c.env.SHORT_IT.put(slug, url, {
+		expirationTtl: c.env.DEFAULT_EXPIRATION,
+		metadata: {
+			owner: c.var.unkey.ownerId,
+		},
+	});
+
+	c.status(201);
+	return c.text(slug);
+});
+
+app.post('/:slug', async (c) => {
+	const { slug } = c.req.param();
+	const url = await c.req.text();
+
+	await c.env.SHORT_IT.put(slug, url, {
+		metadata: {
+			owner: c.var.unkey.ownerId,
+		},
+	});
+
+	c.status(201);
+	return c.text('Created');
+});
+
+app.delete('/:slug', async (c) => {
+	const { slug } = c.req.param();
+	await c.env.SHORT_IT.delete(slug);
+
+	return c.text('Deleted');
+});
+
+export default app;
